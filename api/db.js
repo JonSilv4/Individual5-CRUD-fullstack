5c54b73f1a44d4b8b2ea02de42ae9bf933c1e659
Jonatan Silva <115708859+JonSilv4@users.noreply.github.com>
2023-04-05 09:32:27 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "123456",
+    password: "senac@123",
     database: "crud"
 });
